c29ee67fb0b54197c9676415a216187b8f003080
Adesida Leye <adeleyeadesida@gmail.com>
2023-01-18 11:42:26 +0100

    update on files

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 const itemsNum = document.querySelector('.items span');
 const itemsBtns = document.querySelectorAll('.items div');
 const cartNum = document.querySelector('.cart-count');
+const total = document.querySelector('.total');
+const itemsNumber = document.querySelector('.num');
 const cart = document.querySelector('.cart');
 const cartImg = document.querySelector('.cart-img');
 const addToCart = document.querySelector('.cart-details button')
 const cartContent = document.querySelector('.cart-content');
 
+addToCart.addEventListener('click', addItem);
+
 let count = 0;
+let items = '';
+
 
 itemsBtns.forEach(btn =>{
     btn.addEventListener('click', e =>{
@@ -22,6 +28,8 @@ itemsBtns.forEach(btn =>{
         else{return}
         itemsNum.innerText = count; 
         cartNum.innerText = count;
+        itemsNumber = count;
+        total = `$${125 * count}`;
 
         if (cartNum.innerText > 0){
             cartNum.style.display = 'block';
@@ -31,15 +39,44 @@ itemsBtns.forEach(btn =>{
         }
     })
 })
-//probshere
+
 function openCart(openedCart){
     let openCartVar = openedCart.parentElement.lastElementChild.firstElementChild
     openCartVar.classList.add('open');
     
-    // document.addEventListener('click', e =>{
-    //     if(e.target.tagName == 'DiV'){
-    //         openCartVar.classList.remove('open');
-    //     }
-    // }
-    // )
+    document.addEventListener('click', e =>{
+        if(e.target.classList.contains('body')){
+            openCartVar.classList.remove('open');
+        }
+    }
+    )
 } 
+
+function addItem(){
+    items += `<div class="cart">
+                <p>Cart</p>
+                <div class="">
+                    <img src="./images/image-product-1-thumbnail.jpg" alt="" >
+
+                    <div class="infos">
+                        <p>Fall Limited Edition Sneakers</p>
+                        <p>$125 x <span class="num">${itemsNumber}</span><span class="total">${total}</span></p>
+                    </div>
+
+                    <img class="delete" src="./images/icon-delete.svg" alt="">
+                </div>
+                <button>Checkout</button>
+            </div>`
+    cartContent.innerHTML = items;           
+}
+
+cartContent.addEventListener('click', e =>{
+    if(e.target.classList.contains('delete')){
+        emptyCart = '';
+        emptyCart += `<div class="cart">
+                        <p>Cart</p>
+                        <span class="empty-msg">Your cart is empty</span>
+                    </div>`
+        cartContent.innerHTML = emptyCart;
+    }
+})
